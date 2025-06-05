3034f5abc55fc437b3a8d5835cddcce1555b74dc
Artur Nayman <arturnaiman@gmail.com>
2025-06-05 09:58:33 +0300

    Added exe file to easier launch

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
-icon_directory = r"C:\Users\artur\Documents\pycharm_projects\icon-changer\static\icons"
+import sys
+import os
+
+if getattr(sys, 'frozen', False):
+    base_path = sys._MEIPASS
+else:
+    base_path = os.path.abspath(".")
+
+icon_directory = os.path.join(base_path, 'static', 'icons')
 desktop_path = r"C:\Users\artur\Desktop"
